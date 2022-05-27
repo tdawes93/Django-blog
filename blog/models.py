b4a1207cba01f9a837276bc710f6579d0c786ab8
tdawes93 <tdawes136@googlemail.com>
2022-05-27 16:13:21 +0000

    Add the admin post and comment classes

--- blog/models.py
+++ blog/models.py
@@ -30,7 +30,7 @@ class Post(models.Model):
         return self.likes.count()
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
--- codestar/settings.py
+++ codestar/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
-
 ]
 
 MIDDLEWARE = [
